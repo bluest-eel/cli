2e62eca105d7e6431185c02c55c21f86f17270df
Duncan McGreggor <oubiwann@gmail.com>
2019-11-09 11:14:01 -0600

    Support passing the center point as a flagged value.

--- cli/commands/nearst.go
+++ cli/commands/nearst.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"github.com/bluest-eel/state/tool"
 	"github.com/golang/geo/s2"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -21,13 +22,20 @@ type Nearest struct {
 
 func init() {
 	rootCmd.AddCommand(nearestCmd)
+	nearestCmd.Flags().StringVarP(&rawCenter, "center", "",
+		"60.1699::24.9384::Helsinki Center",
+		"double-colon separated center point data of the format 'lat::long::description'")
 }
 
+var rawCenter string
 var nearestCmd = &cobra.Command{
 	Use:   "nearest",
 	Short: "Get the nearest points",
 	Long:  "Get the nearest points",
 	Run: func(cmd *cobra.Command, args []string) {
-		tool.Run()
+		log.Debug("Args:", args)
+		log.Debug("Center:", rawCenter)
+		log.Tracef("cmd: %#v", cmd)
+		tool.Run(rawCenter)
 	},
 }
