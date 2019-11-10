279c64009c43941083a2a83d4038383c7f3d8a9c
Duncan McGreggor <oubiwann@gmail.com>
2019-11-09 21:39:44 -0600

    Added support for more options and control with flags.

--- cli/commands/nearst.go
+++ cli/commands/nearst.go
@@ -7,6 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	capRadius float64
+	maxCells  int
+	maxLevel  int
+	rawCenter string
+	rawPoints string
+)
+
 // Location data
 type Location struct {
 	Lat    float32
@@ -22,24 +30,29 @@ type Nearest struct {
 
 func init() {
 	rootCmd.AddCommand(nearestCmd)
+	nearestCmd.Flags().Float64VarP(&capRadius, "radius", "r", 20,
+		"The cap radius used to determine nearness, in kilometers")
 	nearestCmd.Flags().StringVarP(&rawCenter, "center", "",
 		"60.1699::24.9384::Helsinki Center",
 		"double-colon separated center point data of the format\n"+
 			"'lat::long::description'; additionally, you may choose\n"+
 			"to pass one of the following pre-defined points:\n"+
 			"  * "+tool.Tolkien+"\n")
+	nearestCmd.Flags().IntVarP(&maxCells, "max-cells", "", 8,
+		"The maximum desired number of cells in the approximation")
+	nearestCmd.Flags().IntVarP(&maxLevel, "max-levels", "", 20,
+		"The maximum cell level to be used")
 	nearestCmd.Flags().StringVarP(&rawPoints, "points", "p",
-		"helskini",
+		tool.Helsinki,
 		"vertical-bar separated list of points of the format\n"+
 			"'point|point|...' where each point has the same format\n"+
 			"as the 'center' flag above; additionally, you may choose\n"+
 			"to pass one of the following pre-defined example points:\n"+
 			"  * "+tool.Helsinki+"\n"+
-			"  * "+tool.Oxford+"\n")
+			"  * "+tool.Oxford+"\n"+
+			"  * "+tool.OxfordPubs+"\n")
 }
 
-var rawCenter string
-var rawPoints string
 var nearestCmd = &cobra.Command{
 	Use:   "nearest",
 	Short: "Get the nearest points",
@@ -48,6 +61,12 @@ var nearestCmd = &cobra.Command{
 		log.Debug("Args:", args)
 		log.Debug("Center:", rawCenter)
 		log.Tracef("cmd: %#v", cmd)
-		tool.Run(rawCenter, rawPoints)
+		tool.Run(&tool.RunOptions{
+			CapRadius:   capRadius,
+			CenterPoint: rawCenter,
+			MaxCells:    maxCells,
+			MaxLevel:    maxLevel,
+			Points:      rawPoints,
+		})
 	},
 }
